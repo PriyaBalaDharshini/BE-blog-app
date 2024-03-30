5cc6da1018edf9d911af5a659f057e3255940ae2
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-03-30 15:27:05 +0530

    Completed

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import authRoute from './routes/authRoute.js'
 import userRoute from './routes/userRoute.js'
 import postRoute from './routes/postRoute.js'
 import categoryRoute from './routes/categoryRoute.js'
+import multer from 'multer'
+
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -16,6 +18,21 @@ app.use(express.json());
 mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
     .then(() => console.log("Database connected successfully"))
     .catch((error) => console.log(error))
+
+//Its gonna take our file and save in images folder i.e. destination and the filename will the name from  req.body 
+const ourStorage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "images")
+    }, filename: (req, file, callback) => {
+        callback(null, "hello.jpg")
+    }
+});
+//uploading file
+const upload = multer({ storage: ourStorage })
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.status(200).send({ message: "File has been uploaded" })
+})
+
 app.get("/", (req, res) => res.send("Welcome to blog app backend"))
 
 app.use("/auth", authRoute);
